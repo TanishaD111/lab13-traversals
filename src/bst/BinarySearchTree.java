4442b73f08c23b3718e0adac88f4cc5a07de9f60
JanAbigail Acosta <janabigail3acosta@gmail.com>
2022-05-06 10:02:30 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -196,6 +196,15 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			postHelper.push(root);
 			while(!postHelper.isEmpty()) {
 				//how should post and postHelper be updated?
+				BSTNode<T> n = postHelper.pop();
+				post.push(n);
+				if(n.leftChild != null) {
+					postHelper.push(n.leftChild);
+				}
+				if(n.rightChild!=null) {
+					postHelper.push(n.rightChild);
+				}
+				
 			}
 			
 			while(!post.isEmpty()) {
